f0d3ebd00e4427c9f8da086f130f96b23996ac3f
jaycee808 <jamie.clark@live.co.uk>
2023-03-04 15:11:10 +0000

    added pages and styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Projects from './components/pages/Projects';
+import Contact from './components/pages/Contact';
 
 function App() {
   return (
-    <>
-      <Navbar />  
-    </>
+    <Router>
+      <div>
+        <Navbar />
+        {/* Wrap Route elements in a Routes component */}
+        <Routes>
+          {/* Define routes using the Route component to render different page components at different paths */}
+          {/* Define a default route that will render the Home component */}
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="projects" element={<Projects />} />
+          {/* Define a route that will have descendant routes */}
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,64 +1,136 @@
 import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
-const Navbar = () => {
+// const NavbarOne = () => {
+//     const [showLinks, setShowLinks] = useState(false);
+
+//     const handleToggle = () => {
+//         setShowLinks(!showLinks);
+//     };
+
+//     return (
+//         <nav id="navbar">
+//         <div className="nav-center">
+        
+//             <div className="nav-header">
+//                 <h1>Jamie Clark</h1>
+//                 <button className="nav-toggle" onClick={handleToggle}>
+//                     <i className="fas fa-bars"></i>
+//                 </button>
+//             </div>
+
+//         <div
+//             id="menu-links"
+//             className={`links-container ${showLinks ? 'show-container' : ''}`}
+//         >
+//             <ul className="links">
+//                 <li>
+//                     <a href="#">About</a>
+//                 </li>
+//                 <li>
+//                     <a href="#">Projects</a>
+//                 </li>
+//                 <li>
+//                     <a href="#">Contact</a>
+//                 </li>
+//             </ul>
+//         </div>
+
+//         <div
+//             id="social-icons"
+//             className={`links-container ${showLinks ? 'show-container' : ''}`}
+//         >
+//             <ul className="links">
+//                 <li>
+//                     <a href="https://github.com/jaycee808" target="_blank" rel="noopener noreferrer">
+//                     <i className="fab fa-github"></i>
+//                     </a>
+//                 </li>
+//                 <li>
+//                     <a href="#">
+//                     <i className="fab fa-linkedin"></i>
+//                     </a>
+//                 </li>
+//                 <li>
+//                     <a href="#">
+//                     <i className="fas fa-envelope"></i>
+//                     </a>
+//                 </li>
+//             </ul>
+//         </div>
+//         </div>
+//     </nav>
+//     );
+// };
+
+function Navbar () {
+
     const [showLinks, setShowLinks] = useState(false);
 
     const handleToggle = () => {
         setShowLinks(!showLinks);
     };
-
+    
     return (
-        <nav id="navbar">
-        <div className="nav-center">
-            <div className="nav-header">
-                <h1>Jamie Clark</h1>
+        // <div id="navbar">
+            <ul id="navbar" className="nav-header">
+                <li className="nav-item">
+                    <NavLink
+                    to="/"
+                    end
+                    className={({ isActive }) =>
+                        isActive ? 'nav-link active' : 'nav-link'
+                    }
+                    >
+                    <h1>JC</h1>
+                    </NavLink>
+                </li>
+
+                <div
+                id="menu-links"
+                className={`links-container ${showLinks ? 'show-container' : ''}`}>
+
+                <li className="nav-item">
+                    <NavLink
+                    to="about"
+                    className={({ isActive }) =>
+                        isActive ? 'nav-link active' : 'nav-link'
+                    }
+                    >
+                    About
+                    </NavLink>
+                </li>
+
+                <li className="nav-item">
+                    <NavLink
+                    to="projects"
+                    className={({ isActive }) =>
+                        isActive ? 'nav-link active' : 'nav-link'
+                    }
+                    >
+                    Projects
+                    </NavLink>
+                </li>
+
+                <li className="nav-item">
+                    <NavLink
+                    to="contact"
+                    end
+                    className={({ isActive }) =>
+                        isActive ? 'nav-link active' : 'nav-link'
+                    }
+                    >
+                    Contact
+                    </NavLink>
+                </li>
+                </div>
                 <button className="nav-toggle" onClick={handleToggle}>
                     <i className="fas fa-bars"></i>
                 </button>
-            </div>
-
-        <div
-            id="menu-links"
-            className={`links-container ${showLinks ? 'show-container' : ''}`}
-        >
-            <ul className="links">
-                <li>
-                    <a href="#">About</a>
-                </li>
-                <li>
-                    <a href="#">Projects</a>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li>
             </ul>
-        </div>
-
-        <div
-            id="social-icons"
-            className={`links-container ${showLinks ? 'show-container' : ''}`}
-        >
-            <ul className="links">
-                <li>
-                    <a href="https://github.com/jaycee808" target="_blank" rel="noopener noreferrer">
-                    <i className="fab fa-github"></i>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                    <i className="fab fa-linkedin"></i>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                    <i className="fas fa-envelope"></i>
-                    </a>
-                </li>
-            </ul>
-        </div>
-        </div>
-    </nav>
+        // </div>
     );
-};
+}
+
 
 export default Navbar;
